2bdf1302d4b6f82b6caa92a3f1dc45d758bf7e97
bchauhan <bchauhan@iastate.edu>
2018-03-19 22:46:09 -0500

    tried server implementation

---
+++ udp_streamer.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define BUFFER 1024
+
+// Global Variables
+int socketsd;
+int socketcd;
+int sourcePort;
+int sourceLen;
+int destPort;
+int destLen;
+struct sockaddr_in sourceaddr;
+struct sockaddr_in destaddr;
+struct hostent *host;
+char buf[BUFFER];
+char *hostaddr;
+int flag;
+int msgsize;
+
+// Server Methods
+void s_socket();
+void s_bind();
---
+++ udpstreamer.c
@@ -0,0 +1,100 @@
+#include "udp_streamer.h"
+/*
+*   Created by Bhimesh Chauhan
+*/
+
+// error detection
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+void s_socket()
+{
+
+    socketsd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (socketsd < 0)
+    {
+        error("ERROR opening socket");
+    }
+    socketcd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (socketcd < 0)
+    {
+        error("ERROR opening socket");
+    }
+}
+
+void s_bind()
+{
+    flag = 1;
+    setsockopt(socketsd, SOL_SOCKET, SO_REUSEADDR,
+         (const void *)&flag , sizeof(int));
+    bzero((char *) &sourceaddr, sizeof(sourceaddr));
+    sourceaddr.sin_family = AF_INET;
+    sourceaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    sourceaddr.sin_port = htons((unsigned short)sourcePort);
+    if (bind(socketsd, (struct sockaddr *) &sourceaddr, sizeof(sourceaddr)) < 0)
+    {
+        error("ERROR on binding");
+    }
+    
+}
+
+
+
+int main(int argc, char **argv)
+{
+
+    // Check Command Line input
+    if (argc != 6)
+    {
+        fprintf(stderr, "usage: %s <Source IP> <Source Port> <Destination IP> <Destination Port> <Loss Rate>\n", argv[0]);
+        exit(1);
+    }
+    
+    sourcePort = atoi(argv[1]);
+    destPort = atoi(argv[3]);
+    
+    // Server: Create Socket
+    s_socket();
+    // Server: Bind Socket
+    s_bind();
+    destLen = sizeof(destaddr);
+    sourceLen = sizeof(sourceaddr);
+    // Main Loop
+    
+    while(1){
+        
+        // Recieve from
+        bzero(buf, BUFFER);
+        msgsize = recvfrom(socketsd, buf, BUFFER, 0,
+         (struct sockaddr *) &sourceaddr, &sourceLen);
+        if (msgsize < 0)
+            error("ERROR in recvfrom");
+        
+        
+        // Get Address of the Host
+        host = gethostbyaddr((const char *)&destaddr.sin_addr.s_addr,
+              sizeof(destaddr.sin_addr.s_addr), AF_INET);
+        if (host == NULL)
+          error("ERROR on gethostbyaddr");
+        hostaddr = inet_ntoa(destaddr.sin_addr);
+        if (hostaddr == NULL)
+          error("ERROR on inet_ntoa\n");
+        printf("server received datagram from %s (%s)\n",
+           host->h_name, hostaddr);
+        printf("server received %lu/%d bytes: %s\n", strlen(buf), msgsize, buf);
+        
+        
+        // Send to with Loss Rate
+        msgsize = sendto(socketsd, buf, strlen(buf), 0,
+           (struct sockaddr *) &destaddr, destLen);
+        if (msgsize < 0)
+          error("ERROR in sendto");
+        
+    }
+    return 0;
+    
+}
